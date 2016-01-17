72a1be963563757568282704116699c5b98e7232
mapopa <gmapopa@gmail.com>
2016-01-17 10:18:40 +0200

    Added security mechanism to login and logout. Can only add/edit/delete posts if logged in

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from . import views
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
@@ -8,7 +11,9 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
-    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),   
+    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), 
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),  
 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
+from django.contrib.auth.decorators import login_required
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
@@ -15,10 +17,12 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -32,6 +36,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -46,15 +51,18 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('blog.views.post_detail', pk=pk)
 
+
 def publish(self):
     self.published_date = timezone.now()
     self.save()
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
